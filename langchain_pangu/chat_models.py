c375ec0efb712570b8a24001305c9739c01b8a20
MeiK <meik2333@gmail.com>
2024-09-07 15:03:22 +0800

    支持 langchain tools

--- langchain_pangu/chat_models.py
+++ langchain_pangu/chat_models.py
@@ -38,13 +38,19 @@ from langchain_core.runnables import Runnable
 from langchain_core.tools import BaseTool
 from requests.exceptions import ChunkedEncodingError
 
-from langchain_pangu.pangukitsappdev.agent.react_pangu_agent import ReactPanguAgent
 from langchain_pangu.pangukitsappdev.api.common_config import AUTH_TOKEN_HEADER
-from langchain_pangu.pangukitsappdev.api.llms.base import get_llm_params, ConversationMessage, Role
-from langchain_pangu.pangukitsappdev.api.llms.factory import LLMs
+from langchain_pangu.pangukitsappdev.api.llms.base import (
+    get_llm_params,
+    ConversationMessage,
+    Role,
+)
 from langchain_pangu.pangukitsappdev.api.llms.llm_config import LLMConfig
 from langchain_pangu.pangukitsappdev.api.tool.base import AbstractTool
-from langchain_pangu.pangukitsappdev.auth.iam import IAMTokenProvider, IAMTokenProviderFactory
+from langchain_pangu.pangukitsappdev.auth.iam import (
+    IAMTokenProvider,
+    IAMTokenProviderFactory,
+)
+from langchain_pangu.tool_calls import PanguToolCalls
 
 
 def _pangu_messages(messages: List[BaseMessage]):
@@ -73,12 +79,28 @@ class ChatPanGu(BaseChatModel):
     pangu_url: Optional[str]
     token_getter: Optional[IAMTokenProvider]
     with_prompt: Optional[bool]
-    tools_agent: Optional[ReactPanguAgent] = None
+    tool_calls: Optional[PanguToolCalls]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.pangu_url: str = self.llm_config.llm_module_config.url
         self.token_getter = IAMTokenProviderFactory.create(self.llm_config.iam_config)
+        self.tool_calls = PanguToolCalls(self.llm_config)
+
+    def _request_body_with_prompt(self, prompt: str):
+        return {
+            "messages": [{"role": "user", "content": prompt}],
+            "stream": False,
+            **get_llm_params(
+                {
+                    "temperature": self.temperature,
+                    "max_tokens": self.max_tokens,
+                    "top_p": self.top_p,
+                    "presence_penalty": self.presence_penalty,
+                    "with_prompt": True,
+                }
+            ),
+        }
 
     def _request_body(self, messages: List[BaseMessage], stream=True):
         rsp = {
@@ -106,20 +128,20 @@ class ChatPanGu(BaseChatModel):
         return headers
 
     async def _astream(
-            self,
-            messages: List[BaseMessage],
-            stop: Optional[List[str]] = None,
-            run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
-            **kwargs: Any,
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+        **kwargs: Any,
     ) -> AsyncIterator[ChatGenerationChunk]:
         proto = self.pangu_url.split("://")[0]
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                    self.pangu_url + "/chat/completions",
-                    headers=self._headers(),
-                    json=self._request_body(messages),
-                    verify_ssl=False,
-                    proxy=self.proxies[proto] if proto in self.proxies else None,
+                self.pangu_url + "/chat/completions",
+                headers=self._headers(),
+                json=self._request_body(messages),
+                verify_ssl=False,
+                proxy=self.proxies[proto] if proto in self.proxies else None,
             ) as rsp:
                 while not rsp.closed:
                     line = await rsp.content.readline()
@@ -144,11 +166,11 @@ class ChatPanGu(BaseChatModel):
                         pass
 
     def _stream(
-            self,
-            messages: List[BaseMessage],
-            stop: Optional[List[str]] = None,
-            run_manager: Optional[CallbackManagerForLLMRun] = None,
-            **kwargs: Any,
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
     ) -> Iterator[ChatGenerationChunk]:
         rsp = requests.post(
             self.pangu_url + "/chat/completions",
@@ -187,22 +209,25 @@ class ChatPanGu(BaseChatModel):
         return "pangu_llm"
 
     async def _agenerate(
-            self,
-            messages: List[BaseMessage],
-            stop: Optional[List[str]] = None,
-            run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
-            **kwargs: Any,
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+        **kwargs: Any,
     ) -> ChatResult:
         if "tools" in kwargs:
-            return self.invoke_with_tools(messages, **kwargs)
+            prompt = self.tool_calls.tool_calls_prompt(messages)
+            body = self._request_body_with_prompt(prompt)
+        else:
+            body = self._request_body(messages, stream=False)
         proto = self.pangu_url.split("://")[0]
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                    self.pangu_url + "/chat/completions",
-                    headers=self._headers(),
-                    json=self._request_body(messages, stream=False),
-                    verify_ssl=False,
-                    proxy=self.proxies[proto] if proto in self.proxies else None,
+                self.pangu_url + "/chat/completions",
+                headers=self._headers(),
+                json=body,
+                verify_ssl=False,
+                proxy=self.proxies[proto] if proto in self.proxies else None,
             ) as rsp:
                 if rsp.status == 200:
                     llm_output = await rsp.json()
@@ -213,27 +238,33 @@ class ChatPanGu(BaseChatModel):
                         rsp.status,
                     )
 
+        message = AIMessage(
+            content=text,
+        )
+        if "tools" in kwargs:
+            message.tool_calls = self.tool_calls.tool_calls(text)
         chat_generation = ChatGeneration(
-            message=AIMessage(
-                content=text,
-            ),
+            message=message,
             generation_info=llm_output,
         )
         return ChatResult(generations=[chat_generation])
 
     def _generate(
-            self,
-            messages: List[BaseMessage],
-            stop: Optional[List[str]] = None,
-            run_manager: Optional[CallbackManagerForLLMRun] = None,
-            **kwargs: Any,
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
     ) -> ChatResult:
         if "tools" in kwargs:
-            return self.invoke_with_tools(messages, **kwargs)
+            prompt = self.tool_calls.tool_calls_prompt(messages)
+            body = self._request_body_with_prompt(prompt)
+        else:
+            body = self._request_body(messages, stream=False)
         rsp = requests.post(
             self.pangu_url + "/chat/completions",
             headers=self._headers(),
-            json=self._request_body(messages, stream=False),
+            json=body,
             verify=False,
             stream=False,
             proxies=self.proxies,
@@ -249,31 +280,24 @@ class ChatPanGu(BaseChatModel):
                 rsp.content,
             )
 
+        message = AIMessage(
+            content=text,
+        )
+        if "tools" in kwargs:
+            message.tool_calls = self.tool_calls.tool_calls(text)
         chat_generation = ChatGeneration(
-            message=AIMessage(
-                content=text,
-            ),
+            message=message,
             generation_info=llm_output,
         )
         return ChatResult(generations=[chat_generation])
 
     def bind_tools(
-            self,
-            tools: Sequence[Union[Dict[str, Any], Type, Callable, BaseTool, AbstractTool]],
-            **kwargs: Any,
+        self,
+        tools: Sequence[Union[Dict[str, Any], Type, Callable, BaseTool, AbstractTool]],
+        **kwargs: Any,
     ) -> Runnable[LanguageModelInput, BaseMessage]:
-        if self.tools_agent is None:
-            config = self.llm_config
-            config.llm_param_config.with_prompt = True
-            # 因为盘古的 tools 实现比较复杂，这里直接调用其原本的实现
-            self.tools_agent = ReactPanguAgent(
-                llm=LLMs.of(
-                    "pangu",
-                    llm_config=config,
-                )
-            )
-            for tool in tools:
-                self.tools_agent.add_tool(tool)
+        for tool in tools:
+            self.tool_calls.add_tool(tool)
         return super().bind(tools=tools, **kwargs)
 
     @staticmethod
@@ -287,19 +311,3 @@ class ChatPanGu(BaseChatModel):
         else:
             role = Role.USER
         return role
-
-    def invoke_with_tools(self, messages: List[BaseMessage], **kwargs) -> ChatResult:
-        msgs: list[ConversationMessage] = []
-        for msg in messages:
-            role = self._message_role(msg)
-            config = msg.dict()
-            config.update(type="chat", role=role)
-            msgs.append(ConversationMessage(**config))
-        rsp = self.tools_agent.run(msgs)
-        chat_generation = ChatGeneration(
-            message=AIMessage(
-                content=rsp.messages[-1].content,
-            ),
-            generation_info=rsp.dict(),
-        )
-        return ChatResult(generations=[chat_generation])
--- tests/tools.py
+++ tests/tools.py
@@ -1,56 +1,30 @@
 import os
 from typing import Literal
-from typing import Type
 
 from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, START, StateGraph, MessagesState
 from langgraph.prebuilt import ToolNode
-from pydantic import BaseModel, Field
 
 from langchain_pangu import ChatPanGu
 from langchain_pangu.pangukitsappdev.api.llms.llm_config import (
-    LLMConfig, LLMModuleConfig,
+    LLMConfig,
+    LLMModuleConfig,
 )
-from langchain_pangu.pangukitsappdev.tool.tool import Tool
 
 os.environ["SDK_CONFIG_PATH"] = "./llm.properties"
 
 
-class Meeting(Tool):
-    class MeetingParam(BaseModel):
-        start: str = Field(description="会议开始时间，格式为yyyy-MM-dd HH:mm")
-        end: str = Field(description="会议结束时间，格式为yyyy-MM-dd HH:mm")
-        meetingRoom: str = Field(description="会议室")
-
-    name = "reserve_meeting_room"
-    description = "预订会议室"
-    args_schema: Type[BaseModel] = MeetingParam
-    principle = "请在需要预订会议室时调用此工具"
-    input_desc = "会议开始与结束时间"
-    output_desc = "会议预订的结果"
-
-    def _run(self, start: str, end: str, meetingRoom: str) -> str:
-        return "预订成功"
+@tool
+def search(city: str):
+    """
+    查询指定城市天气
+    """
+    return f"{city} 今天在下刀子"
 
 
-class Weather(Tool):
-    class WeatherParam(BaseModel):
-        city: str = Field(description="要查询的城市")
-
-    name = "weather"
-    description = "查询天气"
-    args_schema: Type[BaseModel] = WeatherParam
-    principle = "请在需要查询城市天气时调用此工具"
-    input_desc = "要查询的城市"
-    output_desc = "城市的天气"
-
-    def _run(self, city: str) -> str:
-        print("查天气")
-        return "下刀子雨"
-
-
-tools = [Meeting(), Weather()]
+tools = [search]
 
 tool_node = ToolNode(tools)
 
@@ -62,8 +36,8 @@ model = ChatPanGu(
 
 
 # Define the function that determines whether to continue or not
-def should_continue(state: MessagesState) -> Literal['tools', END]:
-    messages = state['messages']
+def should_continue(state: MessagesState) -> Literal["tools", END]:
+    messages = state["messages"]
     last_message = messages[-1]
     # If the LLM makes a tool call, then we route to the "tools" node
     if last_message.tool_calls:
@@ -74,7 +48,7 @@ def should_continue(state: MessagesState) -> Literal['tools', END]:
 
 # Define the function that calls the model
 def call_model(state: MessagesState):
-    messages = state['messages']
+    messages = state["messages"]
     response = model.invoke(messages)
     # We return a list, because this will get added to the existing list
     return {"messages": [response]}
@@ -102,7 +76,7 @@ workflow.add_conditional_edges(
 
 # We now add a normal edge from `tools` to `agent`.
 # This means that after `tools` is called, `agent` node is called next.
-workflow.add_edge("tools", 'agent')
+workflow.add_edge("tools", "agent")
 
 # Initialize memory to persist state between graph runs
 checkpointer = MemorySaver()
@@ -116,6 +90,6 @@ app = workflow.compile(checkpointer=checkpointer)
 # Use the Runnable
 final_state = app.invoke(
     {"messages": [HumanMessage(content="北京天气如何？")]},
-    config={"configurable": {"thread_id": 42}}
+    config={"configurable": {"thread_id": 42}},
 )
 print(final_state["messages"][-1].content)  # 北京现在下刀子雨，请带好雨具。
